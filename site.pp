be39297c8beabdb05db368c941af95972d1f7878
root <root@master.local>
2022-01-13 15:56:00 +0000

    hieraupdt

--- site.pp
+++ site.pp
@@ -1,15 +1,3 @@
 node agent1.local {
-  # Find the first instance of `classes` in hiera data and includes unique values. Does not merge results.
-  $classes = lookup('classes', Variant[String,Array[String]])
-  case $classes {
-    String[1]: {
-      include $classes
-    }
-    Array[String[1],1]: {
-      $classes.unique.include
-    }
-    default: {
-      fail('This node did not receive any classification')
-    }
-  }
+  include ntp
 }
