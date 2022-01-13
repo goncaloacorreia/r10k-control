2c02c47856d3f147b34d930a11959c822495e71b
root <root@master.local>
2022-01-13 15:48:20 +0000

    hieraupdt

--- site.pp
+++ site.pp
@@ -1,3 +1,15 @@
 node default {
-  lookup('classes').include
+  # Find the first instance of `classes` in hiera data and includes unique values. Does not merge results.
+  $classes = lookup('classes', Variant[String,Array[String]])
+  case $classes {
+    String[1]: {
+      include $classes
+    }
+    Array[String[1],1]: {
+      $classes.unique.include
+    }
+    default: {
+      fail('This node did not receive any classification')
+    }
+  }
 }
