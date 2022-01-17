d6f17416b52fee116823cfd74b421f663ac9bd67
root <root@master.local>
2022-01-17 12:32:14 +0000

    c

--- site.pp
+++ site.pp
@@ -6,8 +6,7 @@ node agent1.local {
 class { 'ntp':
   servers => [ '0.europe.pool.ntp.org',
   '1.europe.pool.ntp.org',
-  '2.europe.pool.ntp.org',
-  '3.europe.pool.ntp.org' ],
+  '2.europe.pool.ntp.org' ],
 }
 
 
