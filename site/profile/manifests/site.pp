61f5ce323c6f3af12e9ee43e4baeaa587f53db71
root <root@master.local>
2022-01-17 15:03:44 +0000

    changesitepp

--- site/profile/manifests/site.pp
+++ site/profile/manifests/site.pp
@@ -4,6 +4,14 @@ node agent1.local {
   hiera_include('classes')
 }
 
+class { 'ntp':
+  servers => [ '0.europe.pool.ntp.org',
+  '1.europe.pool.ntp.org',
+  '2.europe.pool.ntp.org',
+  '3.europe.pool.ntp.org' ],
+}
+
+
 
 
 
