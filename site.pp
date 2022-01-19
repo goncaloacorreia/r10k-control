db38033034eb25df9c72d6ebfc48f891a405e534
root <root@master.local>
2022-01-19 17:27:21 +0000

    hieraupdt

--- site.pp
+++ site.pp
@@ -2,10 +2,4 @@ node agent1.local {
   include ntp
 }
 
-class { 'ntp':
-  servers => [ '0.europe.pool.ntp.org',
-  '1.europe.pool.ntp.org',
-  '2.europe.pool.ntp.org',
-  '3.europe.pool.ntp.org' ],
-}
 
