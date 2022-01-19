25af9c4657b0025bfac760818a481c7f9428cd0f
root <root@master.local>
2022-01-19 12:45:35 +0000

    brancht1

--- site.pp
+++ site.pp
@@ -1,11 +1,2 @@
-node agent1.local {
-  include ntp
-}
-
-class { 'ntp':
-  servers => [ '0.europe.pool.ntp.org',
-  '1.europe.pool.ntp.org',
-  '2.europe.pool.ntp.org',
-  '3.europe.pool.ntp.org' ],
-}
+hiera_include('classes')
 
